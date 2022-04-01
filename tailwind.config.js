323829163ace2046cd41336c37df395a5ed2af64
Brooke <brookepaglia@gmail.com>
2022-03-31 20:00:19 -0400

    add styling for single item

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./public/javascript/*.js", "./views/**/*.handlebars"],
   theme: {
-    extend: {},
+    colors: {
+      offwhite: "#f2f2f2",
+      pink: "#d941bf",
+      purple: "#6e34bf",
+      "dark-purple": "#1d1c40",
+      yellow: "#f29f05",
+    },
+    extend: {
+      fontFamily: {
+        "press-start": ['"Press Start 2P"', "cursive"],
+        "open-sans": ["'Open Sans'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
