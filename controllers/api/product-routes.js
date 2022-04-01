8b98e6450bf88f999cecb1ec8989a5e9d5a0fc12
Mike <ampgzo2@gmail.com>
2022-04-01 13:13:53 -0400

    add productorder-model

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -55,7 +55,7 @@ router.post('/', (req, res) => {
     category_id: req.body.category_id
   })
   .then((product) => {
-    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    // if there's product orders, we need to create pairings to bulk create in the ProductTag model
     if (req.body.orderIds) {
       const productOrderIdArr = req.body.orderIds.map((order_id) => {
         return {
