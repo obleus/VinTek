3de86522fe3d112c4592b76ab9589d9a7bed576a
mikeydgithub <ampgzo2@gmail.com>
2022-04-04 14:53:43 -0400

    run seeds error

--- models/index.js
+++ models/index.js
@@ -7,17 +7,17 @@ const ProductOrder = require('./ProductOrder')
 
 // Categories have many Products
 Category.hasMany(Product, { foreignKey: 'category_id' })
-
 // Products belongsTo Category
 Product.belongsTo(Category, { foreignKey: 'category_id' })
 
-// Products belongsTo Product Order
-
 // Orders can belong to one user
 Order.belongsTo(User, { foreignKey: 'user_id' })
-
 // User can have many orders
 User.hasMany(Order, { foreignKey: 'order_id' })
+
+// Products belongsTo Product Order
+Product.belongsTo(ProductOrder, {foreignKey: 'product_id' })
+Order.belongsTo(ProductOrder, { foreignKey: 'order_id' })
 
 
 module.exports = { User, Category, Product, Order, ProductOrder };
