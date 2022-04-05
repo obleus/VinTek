037d86ccb8800fe11958f8db37463882e9a3fa30
mikeydgithub <ampgzo2@gmail.com>
2022-04-04 20:20:58 -0400

    deleted commented out code

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -55,47 +55,4 @@ router.get('/', withAuth, (req, res) => {
 
 // create product order
 
-// Not going to be able to make edits to the order. We can if we want to `-_('.')_-` but all sales are final right?
-
-// router.get('/edit/:id', withAuth, (req, res) => {
-//   Post.findByPk(req.params.id, {
-//     attributes: [
-//       'id',
-//       'post_url',
-//       'title',
-//       'created_at',
-//       [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//     ],
-//     include: [
-//       {
-//         model: Comment,
-//         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//         include: {
-//           model: User,
-//           attributes: ['email']
-//         }
-//       },
-//       {
-//         model: User,
-//         attributes: ['email']
-//       }
-//     ]
-//   })
-//     .then(dbPostData => {
-//       if (dbPostData) {
-//         const post = dbPostData.get({ plain: true });
-        
-//         res.render('edit-post', {
-//           post,
-//           loggedIn: true
-//         });
-//       } else {
-//         res.status(404).end();
-//       }
-//     })
-//     .catch(err => {
-//       res.status(500).json(err);
-//     });
-// });
-
 module.exports = router;
