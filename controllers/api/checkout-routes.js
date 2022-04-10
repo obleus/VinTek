06908d271690c9c7f157bcd2467a98688eca9510
mikeydgithub <ampgzo2@gmail.com>
2022-04-09 22:11:20 -0400

    redirect success and page html

--- controllers/api/checkout-routes.js
+++ controllers/api/checkout-routes.js
@@ -19,84 +19,113 @@ router.get('/', (req, res) => {
 });
 
 router.post('/cart', withAuth, (req, res) => {
+  console.log(req.body)
   if (req.session.cart){
-    req.session.cart.push(req.body.product)
+   if(req.session.cart.indexOf(req.body.productID) == -1){
+     req.session.cart.push(req.body.productID)
+     req.session.checkout.push({
+      productID:req.body.productID,
+      price_data: {
+        currency:'usd',
+        product_data: {
+          name:req.body.productName
+        },
+        unit_amount:req.body.productPrice
+      },
+      quantity: 1
+    })
+   } else {
+     req.session.checkout.forEach((element) => {
+      if(element.productID == req.body.productID){
+        element.quantity++
+      }
+     })
+   }
   }
   else {
-    req.session.cart = [req.body.product]
+    req.session.cart = [req.body.productID]
+    req.session.checkout = [{
+      productID:req.body.productID,
+      price_data: {
+        currency:'usd',
+        product_data: {
+          name: req.body.productName
+        },
+        unit_amount:req.body.productPrice
+      },
+      quantity: 1
+    }]
   }
-    console.log(req.session.cart)
+    console.log(req.session.cart,req.session.checkout)
     res.send('success');
 });
 
 router.post('/create-checkout-session', async (req, res) => {
-//   /*process req.session.cart
-//   get all unique values. how many times product 3,4 show up.
-//   query products for those unique keys and subtract by the amount the times those values show up.
-//   example 12 monitors.  */
+  /*process req.session.cart
+  get all unique values. how many times product 3,4 show up.
+  example 12 monitors.  */
 
-  var isUnique = function (arr) {
-    let count = {}
-    arr.forEach(element => {
-      count[element] = (count[element] || 0) + 1
-    })
-  return count
-  };
+  // var isUnique = function (arr) {
+  //   let count = {}
+  //   arr.forEach(element => {
+  //     count[element] = (count[element] || 0) + 1
+  //   })
+  // return count
+  // };
+
+  // let uniqueProducts = isUnique (req.session.cart)
+  // /* query products for uniqueProducts key and subtract unique productsvalue from productquantity */
+  // uniqueProducts.find({}, (err, data) => {
+  //   res.json(data);
+  // })
   
-  let uniqueProducts = isUnique (req.session.cart)
-//   /* query products for uniqueProducts key and subtract unique productsvalue from productquantity */
-  
-//   /* retrieve query information. add to line itemobject array */
-//   // let purchaseProduct = [] // follow syntax of line item where each object is a product that a customer bought.
-//   /*example
-//   LOOK UP FOR IN LOOP, ALLOW YOU TO GRAB KEY AND VALUE
-//   THIS IS PURCHASE PRODUCT 
-//   [
-//       {
-//         price_data: {
-//           currency: 'usd',
-//           // extract values stored from the database
-//           product_data: {
-//             name: 'LG Monitor',
-//           },
-//           unit_amount: 12,
-//         },
-//         quantity: 1,
-//       },
-//       {
-//         price_data: {
-//           currency: 'usd',
-//           // extract values stored from the database
-//           product_data: {
-//             name: 'LG Keyboard',
-//           },
-//           unit_amount: 20,
-//         },
-//         quantity: 3, THIS REPRESENTS uniqueProducts VALUE
-//       },
-//     ]
-//   */
+  /* retrieve query information. add to line itemobject array */
+  // let purchaseProduct = [] 
+  // follow syntax of line item where each object is a product that a customer bought.
+
+  let cartItems = req.session.checkout
+  cartItems.forEach((element,index) => {
+    delete element.productID
+  })
   const session = await stripe.checkout.sessions.create({
-    line_items:
-      [
-      {
-        price_data: {
-          currency: 'usd',
-          // extract values stored from the database
-          product_data: {
-            name: 'Dell Monitor',
-          },
-          unit_amount: 1200,
-        },
-        quantity: 1,
-      },
-      ],
+    line_items: cartItems,
+      // [
+      // {
+      //   price_data: {
+      //     currency: 'usd',
+      //     // extract values stored from the database
+      //     product_data: {
+      //       name: 'Dell Monitor',
+      //     },
+      //     unit_amount: 1200,
+      //   },
+      //   quantity: 1,
+      // },
+      // ],
       mode: 'payment',
-      success_url: "https://example.com/success",
-      cancel_url: "https://example.com/cancel",
+      success_url: "views/success.handlebars",
+      cancel_url: "views/cancel.handlebars",
     })
-  res.redirect(303, session.url);
+    res.redirect(303, session.url);
 });
+
+ /*example
+  LOOK UP FOR IN LOOP, ALLOW YOU TO GRAB KEY AND VALUE
+  THIS IS PURCHASE PRODUCT 
+  [
+    {
+      price_data: {
+        currency: 'usd',
+        // extract values stored from the database
+        product_data: {
+            name: 'LG Keyboard',
+          },
+          unit_amount: 20,
+        },
+        quantity: 3, THIS REPRESENTS uniqueProducts VALUE
+    },
+  ]
+  */
 
 
 
@@ -132,3 +161,11 @@ router.delete('/:id', withAuth, (req, res) => {
 
 
 module.exports = router;
+
+// redirect success and cancel page html
+// create js file to redirect from sucess.html back to homepage using setTimeout.
+// delete the quantity from sql, clear use delete req.session.cart and req.session.checkout
+/*  let cartItems = req.session.checkout
+  cartItems.forEach((element,index) => {
+    delete element.productID
+})*/
--- public/javascript/addToCart.js
+++ public/javascript/addToCart.js
@@ -1,11 +1,17 @@
 var urlSplitArray = window.location.pathname.split('/')
 
 
-document.querySelector(".checkout").addEventListener('click', () => {
+document.querySelector(".checkout").addEventListener('click', (event) => {
+    let productName = document.getElementById('product-name').textContent
+    let productPrice = document.getElementById('product-price').textContent.split("$")[1] + "00"
+    console.dir(productPrice)
+    console.log(productName, productPrice)
     fetch('/api/checkout/cart', {
         method: 'POST',
         body: JSON.stringify({
-            product:urlSplitArray[urlSplitArray.length - 1]
+            productID:urlSplitArray[urlSplitArray.length - 1],
+            productName:productName,
+            productPrice:productPrice
         }),
         headers: {
             'Content-Type': 'application/json'
