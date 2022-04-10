78977930e7ca174dcfc43651b77916c50614a5d3
Brooke <brookepaglia@gmail.com>
2022-04-10 19:23:23 -0400

    update success routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,19 +1,18 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const userRoutes = require('./user-routes.js');
-const productRoutes = require('./product-routes');
-const categoryRoutes = require('./category-routes');
-const productOrderRoutes = require('./product-order-routes');
-const checkoutRoutes = require('./checkout-routes');
-const successRoutes = require('./success-routes')
-
+const userRoutes = require("./user-routes.js");
+const productRoutes = require("./product-routes");
+const categoryRoutes = require("./category-routes");
+const productOrderRoutes = require("./product-order-routes");
+const checkoutRoutes = require("./checkout-routes");
+const successRoutes = require("./success-routes");
 
 // name endpoint for routes
-router.use('/users', userRoutes);
-router.use('/products', productRoutes);
-router.use('/categories', categoryRoutes);
-router.use('/productorders', productOrderRoutes);
-router.use('/checkout', checkoutRoutes);
-router.use('/success', successRoutes);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/productorders", productOrderRoutes);
+router.use("/checkout", checkoutRoutes);
+router.use("/success", successRoutes);
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const homeRoutes = require("./home-routes.js");
 const apiRoutes = require("./api/");
 const dashboardRoutes = require("./dashboard-routes.js");
+const successRoutes = require("./success-routes");
 
 router.use("/dashboard", dashboardRoutes);
+router.use("/success", successRoutes);
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
 
